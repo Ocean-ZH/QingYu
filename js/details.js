3b58fe65f20f87f5d8f053de827f25cd1899f610
Ocean-ZH <fdy409784@outlook.com>
2016-12-14 23:10:40 +0800

    details page
    details page developing

--- js/details.js
+++ js/details.js
@@ -2,10 +2,36 @@
 * @Author: Marte
 * @Date:   2016-11-04 19:47:22
 * @Last Modified by:   Marte
-* @Last Modified time: 2016-12-13 16:30:59
+* @Last Modified time: 2016-12-14 23:05:58
 */
 
 $(document).ready(function(){
+
+/*底部导航显隐开始*/
+    var winH=$(window).height();
+    var headerH=$('.header').height();
+    var winSTbefore=0;
+    $(window).scroll(function(event) {
+        var winST=$(window).scrollTop();
+        var docH=$(document).height();
+        if(winST<headerH){
+            $('.tabBars').removeClass('tBBhide');
+        }else if((winST+winH)==docH){
+            $('.tabBars').removeClass('tBBhide');
+        }else if(winST>winSTbefore){
+            if($('.tabBars').hasClass('tBBhide')!=true){
+                $('.tabBars').addClass('tBBhide');
+            }
+            winSTbefore=winST;
+        }else if(winST<winSTbefore){
+            if($('.tabBars').hasClass('tBBhide')){
+                $('.tabBars').removeClass('tBBhide');
+            }
+            winSTbefore=winST;
+        }
+    });
+/*底部导航显隐结束*/
+
     $('.tabBars ul li').click(function(event) {
         $(this).addClass('current').siblings().removeClass('current');
         var tabNum = $(this).index();
--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2016-11-04 19:47:22
 * @Last Modified by:   Marte
-* @Last Modified time: 2016-11-06 11:06:18
+* @Last Modified time: 2016-12-14 23:05:31
 */
 
 $(document).ready(function(){
@@ -14,6 +14,31 @@ $(document).ready(function(){
         autoPlay:true,//自动播放
         autoPage:true //自动分页
     });
+
+/*底部导航显隐开始*/
+    var winH=$(window).height();
+    var headerH=$('.header').height();
+    var winSTbefore=0;
+    $(window).scroll(function(event) {
+        var winST=$(window).scrollTop();
+        var docH=$(document).height();
+        if(winST<headerH){
+            $('.tabBars').removeClass('tBBhide');
+        }else if((winST+winH)==docH){
+            $('.tabBars').removeClass('tBBhide');
+        }else if(winST>winSTbefore){
+            if($('.tabBars').hasClass('tBBhide')!=true){
+                $('.tabBars').addClass('tBBhide');
+            }
+            winSTbefore=winST;
+        }else if(winST<winSTbefore){
+            if($('.tabBars').hasClass('tBBhide')){
+                $('.tabBars').removeClass('tBBhide');
+            }
+            winSTbefore=winST;
+        }
+    });
+/*底部导航显隐结束*/
 
     $('.tabBars ul li').click(function(event) {
         $(this).addClass('current').siblings().removeClass('current');
